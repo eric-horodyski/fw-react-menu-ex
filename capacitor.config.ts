de58dd2a6edd053a5a0f4617664bbf3a9205f5a2
Eric Horodyski <horodyski@ionic.io>
2022-03-17 10:47:21 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fw-react-menu-ex',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
